6078769da659949c6759265a03f8a1094e878817
tomlenbzh <thomas.lenoel@epitech.eu>
2020-05-17 17:06:26 +0100

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/website/website.module').then(m => m.WebsiteModule) },
+  {
+    path: 'backoffice',
+    loadChildren: () => import('./pages/backoffice/backoffice.module').then(m => m.BackofficeModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabled',
+      scrollPositionRestoration: 'top'
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { LazyLoadImageModule, scrollPreset } from 'ng-lazyload-image';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CarouselModule,
+    LazyLoadImageModule.forRoot({ preset: scrollPreset })
   ],
-  providers: [],
+  providers: [ScrollToService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
