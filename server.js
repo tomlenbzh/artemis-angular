659c976a96dffc5b077ea3960c463c9ef3eb2faa
tomlenbzh <thomas.lenoel@epitech.eu>
2020-05-17 18:26:52 +0100

    Heroku 5th Build Test

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + 'dist/artemis-angular-app'));
+app.use(express.static(__dirname + '/dist/artemis-angular-app'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname + '/dist/artemis-angular-app/browser/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/artemis-angular-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
